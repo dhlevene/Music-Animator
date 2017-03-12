21eebe1491e53576f7ee2794d5e96cfee4d3c396
dhlevene <dandanlev3@hotmail.com>
2017-03-12 16:53:13 -0400

    First Commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+/* Collaborators:
+* Daniel Levene
+* Damian Suski
+* Mauricio Mendez
+* Marcy Yi
+* Anthony Chand
+* James Harrison*/
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
