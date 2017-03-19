77b74ae624b8677c5ea0ae2d18ecd335ff0c6e9f
Damian <damianthesuski@gmail.com>
2017-03-19 14:49:28 -0400

    Updated with a working frame

--- src/com/music/animator/Main.java
+++ src/com/music/animator/Main.java
@@ -6,9 +6,23 @@ package com.music.animator;/* Collaborators:
 * Anthony Chand
 * James Harrison*/
 
+import javax.swing.*;
+import java.awt.*;
+
+/*Last edit by Damian Suski, 3/19/2017 Added JFrame*/
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        JFrame window = new JFrame("Animator");
+
+        /*Following code is used to get screen resolution. Probably unnecessary*/
+        /*Dimension screensize = Toolkit.getDefaultToolkit().getScreenSize();*/
+        window.setSize(800,600);
+        window.add(new MainScreen());
+
+        window.setResizable(false);
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+        window.setDefaultCloseOperation(3);
     }
 }
--- src/com/music/animator/MainScreen.java
+++ src/com/music/animator/MainScreen.java
@@ -1,13 +1,39 @@
 package com.music.animator;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
- * Created by anthonychand on 3/13/17.
+ * Created by Damian Suski on 3/19/2017.
  */
-public class MainScreen
-{
+public class MainScreen extends JPanel implements ActionListener{
 
+    Timer timer = new Timer(10,this);
 
-    private JPanel MainScreen;
+    public MainScreen(){
+        /*Makes sure that we can click on the panel*/
+        setFocusable(true);
+        requestFocus();
+
+        timer.start();
+    }
+
+    /*Takes care of all the rendering, necessary for drawing the animations*/
+    public void paint(Graphics graphics){
+        super.paint(graphics);
+
+        /*Paints a black rectangle over the whole window*/
+        graphics.setColor(Color.BLACK);
+        graphics.fillRect(0,0,800,600);
+        graphics.setColor(Color.WHITE);
+        graphics.drawString("What is up homies?",50,50);
+    }
+
+    /*Called every time the timer executes (every 10 millis)*/
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        repaint();
+    }
 }
