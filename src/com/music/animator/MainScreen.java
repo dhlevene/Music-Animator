297212296909a01adc77eb97f210977549bc62e8
Mauricio Mendez <mendez1226@gmail.com>
2017-03-26 20:53:17 -0400

    added a second UI Class that is the next Panel that fills the mainscreen called "DancerSelection" However, that will be refractored(renamed) to contain animation and/or the screenshot screen since the dancer selection and audio selection can both be done within the MainScreen

--- src/com/music/animator/MainScreen.java
+++ src/com/music/animator/MainScreen.java
@@ -11,17 +11,32 @@ import java.awt.event.ActionListener;
 public class MainScreen extends JPanel implements ActionListener{
 
     Timer timer = new Timer(10,this);
-    private JButton startAnimationButton;
-    private JButton selectDancerButton;
-    private JButton selectAudioButton;
-    private JButton uploadAudioButton;
+
+    private JPanel mainPanel;
     private JPanel top;
     private JPanel left;
     private JPanel right;
     private JPanel bottom;
+    private JButton startAnimationButton;
+    private JButton selectDancerButton;
+    private JButton selectAudioButton;
+    private JButton uploadAudioButton;
+    private JLabel animatedMusicDancer;
+    private JLabel dancer;
+    private JLabel audio;
 
     public MainScreen(){
         init();
+
+        top.add(animatedMusicDancer);
+        right.add(audio);
+        left.add(dancer);
+
+        bottom.add(startAnimationButton);
+        left.add(selectDancerButton);
+        right.add(selectAudioButton);
+        right.add(uploadAudioButton);
+
         //left = new JPanel();
         //add(left);
         //left.add(startAnimationButton);
@@ -36,16 +51,19 @@ public class MainScreen extends JPanel implements ActionListener{
 
     public void init()
     {
+        mainPanel = new JPanel();
         //top = new JPanel(new FlowLayout(FlowLayout.LEADING));
         //left = new JPanel(new BoxLayout(left, BoxLayout.Y_AXIS));
         //right = new JPanel(new BoxLayout(right, BoxLayout.Y_AXIS));
-        //bottom = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        bottom = new JPanel(new FlowLayout(FlowLayout.CENTER));
+
 
         top = new JPanel();
         left = new JPanel();
         right = new JPanel();
-        bottom = new JPanel();
+        //bottom = new JPanel();
 
+        mainPanel.setPreferredSize(new Dimension(800,600));
         top.setPreferredSize(new Dimension(800,100));
         top.setBorder(BorderFactory.createTitledBorder("Music Animator"));
         left.setPreferredSize(new Dimension(350,350));
@@ -55,19 +73,33 @@ public class MainScreen extends JPanel implements ActionListener{
         bottom.setPreferredSize(new Dimension(800,100));
         bottom.setBorder(BorderFactory.createTitledBorder("Start"));
 
-        add(top, BorderLayout.NORTH);
-        add(left, BorderLayout.WEST);
-        add(right, BorderLayout.EAST);
-        add(bottom, BorderLayout.SOUTH);
+        this.add(mainPanel);
+        mainPanel.add(top, BorderLayout.NORTH);
+        mainPanel.add(left, BorderLayout.WEST);
+        mainPanel.add(right, BorderLayout.EAST);
+        mainPanel.add(bottom, BorderLayout.SOUTH);
 
         startAnimationButton = new JButton("Start Animation");
         selectDancerButton = new JButton("Select Dancer");
-        selectAudioButton = new JButton("Select Audio");
-        uploadAudioButton = new JButton("Upload Audio");
-        bottom.add(startAnimationButton);
-        left.add(selectDancerButton);
-        right.add(selectAudioButton);
-        right.add(uploadAudioButton);
+        selectAudioButton = new JButton("Select Song");
+            uploadAudioButton = new JButton("Upload Audio");
+
+        selectDancerButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //this.add(new DancerSelection());
+                remove(mainPanel);
+                add(new DancerSelection());
+                validate();
+            }
+        });
+        startAnimationButton.addActionListener(this);
+        selectAudioButton.addActionListener(this);
+        uploadAudioButton.addActionListener(this);
+
+        animatedMusicDancer = new JLabel("Animated Music Dancer");
+        dancer = new JLabel("Please select a Dancer for the animation");
+        audio = new JLabel("Please select a song or upload an audio file");
     }
 
     /*Takes care of all the rendering, necessary for drawing the animations*/
@@ -82,6 +114,7 @@ public class MainScreen extends JPanel implements ActionListener{
        // graphics.drawString("Please Select Dancer ",100,200);
        // graphics.drawString("Please Select or Upload Audio ",500,200);
     }
+
 
     /*Called every time the timer executes (every 10 millis)*/
     @Override
