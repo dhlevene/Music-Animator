8f92ec270df21dd17b4525976cb725bc41f7f491
James Harrison <jharrison23@knights.ucf.edu>
2017-04-09 00:46:04 -0400

    Commented the screenShotPanel.java file, which sends the email

--- src/com/music/animator/screenShotPanel.java
+++ src/com/music/animator/screenShotPanel.java
@@ -20,16 +20,31 @@ public class screenShotPanel extends JPanel implements ActionListener {
     private static String EMAIL_ADDRESS = "SoFloDev";
     private static String EMAIL_PASSWORD = "Soflodev123";
 
-    JPanel picturePanel;//left
-    JPanel actionPanel;//right
+    // Left Panel
+    JPanel picturePanel;
+
+    // Right panel
+    JPanel actionPanel;
+
+    // Send Email
     JButton emailButton;
+
+    // Back to Animation
     JButton backButton;
+
+    // Back to home screen
     JButton menuButton;
     JLabel emailLabel;
+
+    // Field which stores user email
     JTextField emailInput;
 
-    public screenShotPanel(){
+
+    public screenShotPanel()
+    {
         init();
+
+        // Added the UI elements to the Right panel
         actionPanel.add(emailLabel);
         actionPanel.add(emailInput);
         actionPanel.add(emailButton);
@@ -38,34 +53,51 @@ public class screenShotPanel extends JPanel implements ActionListener {
 
     }
 
-    public void init(){
+    public void init()
+    {
+        // Initialize Panes
         picturePanel = new JPanel();
         actionPanel = new JPanel();
 
+        // Set sizes and titles of JPanels
         picturePanel.setPreferredSize(new Dimension(400,380));
         picturePanel.setBorder(BorderFactory.createTitledBorder("ScreenShot"));
         actionPanel.setPreferredSize(new Dimension(200,380));
-        actionPanel.setBorder(BorderFactory.createTitledBorder("Buttons and stuff"));
+        actionPanel.setBorder(BorderFactory.createTitledBorder("Options"));
 
+        // Set panel to the left of frame
         this.add(picturePanel, BorderLayout.WEST);
+
+        // Set panel to right of frame
         this.add(actionPanel, BorderLayout.EAST);
 
-        emailButton = new JButton("send Email");
+        // Initialize Buttons
+        emailButton = new JButton("Send Email");
+        backButton = new JButton("Back to Animation");
+        menuButton = new JButton("Back to Main Screen");
+
+        emailLabel = new JLabel("Enter email here: ");
+        emailInput = new JTextField("yourEmail@example.com");
+
+        // When the user presses the send email button,
+        // send an email of the screenshot from SoFloDev.gmail to the address recieved from user input
         emailButton.addActionListener(new ActionListener()
         {
             @Override
             public void actionPerformed(ActionEvent e)
             {
+                // Store the recipient address in an array
+                String[] recipientArray = { emailInput.getText() };
 
-                String[] recipientArray = { emailInput.getText() }; // list of recipient email addresses
+                // String representing the body of the email to be sent
                 String body = "Here's the Screenshot from your animation!!";
 
+                // method call to send email, passing in the recipient address and body of message
                 sendScreenshot(recipientArray, body);
 
             }
         });
 
-        backButton = new JButton("Back to Animation");
         backButton.addActionListener(new ActionListener()
         {
             @Override
@@ -77,14 +109,7 @@ public class screenShotPanel extends JPanel implements ActionListener {
         });
 
 
-
-        menuButton = new JButton("Back to Main Screen");
-
-        emailLabel = new JLabel("Enter email here: ");
-        emailInput = new JTextField("yourEmail@example.com");
     }
-
-
 
     @Override
     public void actionPerformed(ActionEvent e) {
@@ -92,56 +117,96 @@ public class screenShotPanel extends JPanel implements ActionListener {
     }
 
 
+    // Method which will send an email from:
+    // SoFloDev.gmail.com to the recipient address entered by the user
     private static void sendScreenshot(String[] recipientArray, String body)
     {
+        // Frame for email sent/error popup
         JFrame emailSentFrame = new JFrame();
 
+        // String variables for the host, port and subject of the email
         String host = "smtp.gmail.com";
         String port = "587";
         String subject = "Your Animation Screenshot";
 
+        // Properties used to create the session
         Properties properties = System.getProperties();
+
+        // Host name of the mail server
         properties.put("mail.smtp.host", host);
-        properties.put("mail.smtp.auth", "true");
+
+        // Username to connect to mail server, email address used to send
         properties.put("mail.smtp.user", EMAIL_ADDRESS);
+
+        // Password for the email address used to send the email
         properties.put("mail.smtp.password", EMAIL_PASSWORD);
+
+        // Authenticate the user
+        properties.put("mail.smtp.auth", "true");
+
+        // Port number of the mail server
         properties.put("mail.smtp.port", port);
+
+        // Enable use of starttls commands
         properties.put("mail.smtp.starttls.enable", "true");
 
+        // New mail session, collects the properties and the defaults used by the mail API
         Session session = Session.getDefaultInstance(properties);
 
+        // Create empty message object
         MimeMessage mimeMessage = new MimeMessage(session);
 
+        // Try to send the email
         try
         {
+
+            // set the "From" header field of the email
             mimeMessage.setFrom(new InternetAddress(EMAIL_ADDRESS));
 
+            // New array of the class internet address to store the recipient email address in the format "user@host.domain"
             InternetAddress[] recipientAddress = new InternetAddress[recipientArray.length];
 
+            // loop through the recipient email string array
             for(int i = 0; i < recipientArray.length; i++)
             {
+                // Copy over to the InternetAddress array
                 recipientAddress[i] = new InternetAddress(recipientArray[i]);
             }
 
+            // Loop through the internet address
             for(int i = 0; i < recipientAddress.length; i++)
             {
+                // Add the address to the recipient type
                 mimeMessage.addRecipient(Message.RecipientType.TO, recipientAddress[i]);
             }
 
+            // Sets the body of the message
             mimeMessage.setText(body);
+
+            // set the "Subject" Header field
             mimeMessage.setSubject(subject);
 
+            // Get a transport which implements smtp
             Transport transport = session.getTransport("smtp");
+
+            // Connect transport to specified sending address
             transport.connect(host, EMAIL_ADDRESS, EMAIL_PASSWORD);
+
+            // Send the message
             transport.sendMessage(mimeMessage, mimeMessage.getAllRecipients());
+
+            // Close connection
             transport.close();
 
+            // Pop up indicating successful email
             JOptionPane.showMessageDialog(emailSentFrame, "Screenshot has been emailed", "Sent", JOptionPane.PLAIN_MESSAGE);
 
         }
 
+        // Catch an error in sending the email
         catch (MessagingException messagingException)
         {
+            // Pop indicating an error with sending the email
             JOptionPane.showMessageDialog(emailSentFrame, "Email Failed", "Error", JOptionPane.ERROR_MESSAGE);
 
         }
