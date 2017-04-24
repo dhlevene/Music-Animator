97e0fca5828dc37d6b318c64686061efb4dc2e35
James Harrison <jharrison23@knights.ucf.edu>
2017-04-23 21:43:08 -0400

    renamed the variables in screenshot panel

--- src/com/music/animator/screenShotPanel.java
+++ src/com/music/animator/screenShotPanel.java
@@ -18,24 +18,24 @@ import javax.mail.*;
 public class screenShotPanel extends JPanel implements ActionListener {
 
     // Gmail credentials, for SoFloDev@gmail.com with the password
-    private static String EMAIL_ADDRESS = "SoFloDev";
-    private static String EMAIL_PASSWORD = "Soflodev123";
+    private static String _EMAIL_ADDRESS = "SoFloDev";
+    private static String _EMAIL_PASSWORD = "Soflodev123";
     
     // Left Panel
-    JPanel picturePanel;
+    JPanel _picturePanel;
 
     // Right panel
-    JPanel actionPanel;
+    JPanel _actionPanel;
 
     // Send Email
-    JButton emailButton;
+    JButton _emailButton;
 
     // Back to home screen
-    JButton menuButton;
-    JLabel emailLabel;
+    JButton _menuButton;
+    JLabel _emailLabel;
 
     // Field which stores user email
-    JTextField emailInput;
+    JTextField _emailInput;
 
 
     public screenShotPanel()
@@ -43,47 +43,47 @@ public class screenShotPanel extends JPanel implements ActionListener {
         init();
 
         // Added the UI elements to the Right panel
-        actionPanel.add(emailLabel);
-        actionPanel.add(emailInput);
-        actionPanel.add(emailButton);
-        actionPanel.add(menuButton);
+        _actionPanel.add(_emailLabel);
+        _actionPanel.add(_emailInput);
+        _actionPanel.add(_emailButton);
+        _actionPanel.add(_menuButton);
     }
 
     public void init()
     {
 
         // Initialize Panes
-        picturePanel = new JPanel();
-        actionPanel = new JPanel();
+        _picturePanel = new JPanel();
+        _actionPanel = new JPanel();
 
         // Set sizes and titles of JPanels
-        //picturePanel.setPreferredSize(new Dimension(400,380));
-        //picturePanel.setBorder(BorderFactory.createTitledBorder("ScreenShot"));
-        actionPanel.setPreferredSize(new Dimension(200,380));
-        actionPanel.setBorder(BorderFactory.createTitledBorder("Options"));
+        //_picturePanel.setPreferredSize(new Dimension(400,380));
+        //_picturePanel.setBorder(BorderFactory.createTitledBorder("ScreenShot"));
+        _actionPanel.setPreferredSize(new Dimension(200,380));
+        _actionPanel.setBorder(BorderFactory.createTitledBorder("Options"));
 
         // Set panel to the left of frame
-        //this.add(picturePanel, BorderLayout.WEST);
+        //this.add(_picturePanel, BorderLayout.WEST);
 
         // Set panel to right of frame
-        this.add(actionPanel, BorderLayout.EAST);
+        this.add(_actionPanel, BorderLayout.EAST);
 
         // Initialize Buttons
-        emailButton = new JButton("Send Email");
-        menuButton = new JButton("Back to Main Screen");
+        _emailButton = new JButton("Send Email");
+        _menuButton = new JButton("Back to Main Screen");
 
-        emailLabel = new JLabel("Enter email here: ");
-        emailInput = new JTextField("yourEmail@example.com");
+        _emailLabel = new JLabel("Enter email here: ");
+        _emailInput = new JTextField("yourEmail@example.com");
 
         // When the user presses the send email button,
         // send an email of the screenshot from SoFloDev.gmail to the address recieved from user input
-        emailButton.addActionListener(new ActionListener()
+        _emailButton.addActionListener(new ActionListener()
         {
             @Override
             public void actionPerformed(ActionEvent e)
             {
                 // Store the recipient address in an array
-                String[] recipientArray = { emailInput.getText() };
+                String[] recipientArray = { _emailInput.getText() };
 
                 // String representing the body of the email to be sent
                 String body = "Here's the Screenshot from your animation!!";
@@ -95,7 +95,7 @@ public class screenShotPanel extends JPanel implements ActionListener {
         });
 
 
-        menuButton.addActionListener(new ActionListener() {
+        _menuButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
 
@@ -132,10 +132,10 @@ public class screenShotPanel extends JPanel implements ActionListener {
         properties.put("mail.smtp.host", host);
 
         // Username to connect to mail server, email address used to send
-        properties.put("mail.smtp.user", EMAIL_ADDRESS);
+        properties.put("mail.smtp.user", _EMAIL_ADDRESS);
 
         // Password for the email address used to send the email
-        properties.put("mail.smtp.password", EMAIL_PASSWORD);
+        properties.put("mail.smtp.password", _EMAIL_PASSWORD);
 
         // Authenticate the user
         properties.put("mail.smtp.auth", "true");
@@ -151,7 +151,7 @@ public class screenShotPanel extends JPanel implements ActionListener {
         {
             protected PasswordAuthentication getPasswordAuthentication()
             {
-                return new PasswordAuthentication(EMAIL_ADDRESS, EMAIL_PASSWORD);
+                return new PasswordAuthentication(_EMAIL_ADDRESS, _EMAIL_PASSWORD);
             }
         });
 
@@ -164,7 +164,7 @@ public class screenShotPanel extends JPanel implements ActionListener {
         {
 
             // Set the "From" header field of the email.
-            message.setFrom(new InternetAddress(EMAIL_ADDRESS));
+            message.setFrom(new InternetAddress(_EMAIL_ADDRESS));
 
             // New array of the class internet address to store the recipient email address in the format "user@host.domain"
             InternetAddress[] recipientAddress = new InternetAddress[recipientArray.length];
